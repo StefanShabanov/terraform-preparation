335174c082df7eac0c0610f33ecbb61191f626cb
StefanShabanov <shabanov.outlook.com>
2023-09-23 15:33:57 +0300

    first push

---
+++ terraform-backend/main.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+    region = "eu-central-1"
+}
+
+resource "aws_s3_bucket" "stefan-terraform-state" {
+    bucket = "terraform-state"
+
+    #to prevent accidental deletion. Any attempt to delete the bucket will cause TF to exit with error.
+      lifecycle {
+      prevent_destroy = true
+    }
+}
+
+#Adding versioning. Every update to a file in the bucket will create a new version of it
+
+resource "aws_s3_bucket_versioning" "enabled" {
+    bucket = aws_s3_bucket.stefan-terraform-state.id
+    versioning_configuration {
+      status = "Enabled"
+    }
+}
+
+#Using a server-side encryption for all data written on our S3 bucket.
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+    bucket = aws_s3_bucket.stefan-terraform-state.id
+
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+}
+
+#Blocking all public access to the bucket
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+    bucket = aws_s3_bucket.stefan-terraform-state
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
+}
+
+#Creating DynamoDB table for locking. For this I must create a table with a primary key "LockID"
+
+resource "aws_dynamodb_table" "terraform_locks" {
+    name = "stefan-terraform-locks"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+    
+    attribute {
+      name = "LockID"
+      type = "S"
+    }
+}
